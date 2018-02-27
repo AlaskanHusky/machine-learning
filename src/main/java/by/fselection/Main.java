7f3445997b5a2bc2a1987f08f86f7bcac5d0b175
Pavel Yanushevsky <4erep.moroz@mail.ru>
2018-02-27 12:36:09 +0300

    Create Excel file reader

---
+++ src/main/java/by/fselection/Main.java
@@ -0,0 +1,15 @@
+package by.fselection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String fileName = "src/main/resources/data.xlsx";
+        String healthySheet = "Здоровые";
+        String sickSheet = "Больные";
+        String indicatorsSheet = "Признаки";
+        int healthyPeopleNumber = 26;
+        int sickPeopleNumber = 28;
+        FileReaderUtil fileReader = new FileReaderUtil();
+
+    }
+}
